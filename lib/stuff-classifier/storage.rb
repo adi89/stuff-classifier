42ea770ecdc0040237059b374ccca45aae68ce3b
Adi Singh <adityasingh89@gmail.com>
2014-01-17 12:59:30 -0500

    redis

--- lib/stuff-classifier/storage.rb
+++ lib/stuff-classifier/storage.rb
@@ -29,9 +29,9 @@ module StuffClassifier
       to_store = classifier.class.to_store + classifier.class.superclass.to_store
       @storage[classifier.name] =  to_store.inject({}) {|h,var| h[var] = classifier.instance_variable_get("@#{var}");h}
     end
-    
+
     def clear_storage(classifier)
-      @storage.delete(classifier.name)      
+      @storage.delete(classifier.name)
     end
 
   end
@@ -87,4 +87,37 @@ module StuffClassifier
     end
 
   end
+
+   class RedisStorage < Storage
+    def initialize(key, redis_options=nil)
+      super
+      @key = key
+      @redis = Redis.new(redis_options || {})
+    end
+
+    def load_state(classifier)
+      if @storage.length == 0 && @redis.exists(@key)
+        data = @redis.get(@key)
+        @storage = Marshal.load(data)
+      end
+      storage_to_classifier(classifier)
+    end
+
+    def save_state(classifier)
+      classifier_to_storage(classifier)
+      _write_to_redis
+    end
+
+    def purge_state(classifier)
+      clear_storage(classifier)
+      _write_to_redis
+    end
+
+    private
+    def _write_to_redis
+      data = Marshal.dump(@storage)
+      @redis.set(@key, data)
+    end
+  end
+
 end
